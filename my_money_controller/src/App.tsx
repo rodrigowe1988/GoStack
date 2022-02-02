089bac0bb2504d16e1b706194d47ee0f35100c6e
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-02-02 16:23:10 -0300

    money_controleer

--- my_money_controller/src/App.tsx
+++ my_money_controller/src/App.tsx
@@ -1,13 +1,33 @@
 import { Global } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
   return (
     <>
       <Global/>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
