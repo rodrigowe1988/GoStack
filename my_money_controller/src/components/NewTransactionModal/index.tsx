16c0647adeb4069eb29f97dc27fef42c707bb369
Rodrigo Weber <CIANDT\rodrigoweber@lnb034965cps.ciandt.global>
2022-02-10 16:08:38 -0300

    dt_money

--- my_money_controller/src/components/NewTransactionModal/index.tsx
+++ my_money_controller/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
 	isOpen: boolean;
@@ -16,6 +17,17 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 	const [value, setValue] = useState(0);
 	const [category, setCategory] = useState('');
 	const [type, setType] = useState('deposit');
+
+	function handleCreateNewTransaction(event: FormEvent) {
+		event.preventDefault();
+
+		const data = ({
+			title,
+			value,
+			category,
+			type
+		})
+	}
 
 	return (
 		<Modal
@@ -33,14 +45,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 				<img src={closeImg} alt="Fechar modal" />
 			</button>
 
-			<Container>
+			<Container onSubmit={handleCreateNewTransaction}>
 				<h2>Cadastrar Transação</h2>
 				<input
 					placeholder="Título"
-				
+					value={title}
+					onChange={event => setTitle(event.target.value)}
 				/>
 
-				<input placeholder="Valor"/>
+				<input 
+					placeholder="Valor"
+					type="number"
+					value={value}
+					onChange={event => setValue(Number(event.target.value))}
+				/>
 
 				<TransactionTypeContainer>
 					<RadioBox
@@ -63,7 +81,11 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 					</RadioBox>
 				</TransactionTypeContainer>
 
-				<input placeholder="Categoria"/>
+				<input 
+					placeholder="Categoria"
+					value={category}
+					onChange={event => setCategory(event.target.value)}	
+				/>
 
 				<button type="submit">
 					Cadastrar
